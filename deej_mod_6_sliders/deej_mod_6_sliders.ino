6c8858660340f9f821ff14acd4b6d63cf35299d9
Andrew L. Weekes <alweekes@users.noreply.github.com>
2024-04-08 21:12:27 +0100

    Added deej Arduino modified code for 6 sliders
    First PCB release slightly restricts the full movement of the slide potentiometers, so the full 0-1023 output range is not achieved.
    Since there is plenty of resolution added a map and  constrain function to scale the analog read values to produce the full 0-1023 output values.
    Changed order of analog inputs so sliders in config.yaml appear in the correct order.

---
+++ deej_mod_6_sliders/deej_mod_6_sliders.ino
@@ -0,0 +1,58 @@
+const int NUM_SLIDERS = 6;
+const int analogInputs[NUM_SLIDERS] = {A3, A2, A1, A0, A5, A4};
+
+// Address limited mechanical range of sliders
+const int sliderMin = 15;
+const int sliderMax = 1010;
+
+int analogSliderValues[NUM_SLIDERS];
+int analogSliderRead[NUM_SLIDERS];
+
+void setup() { 
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    pinMode(analogInputs[i], INPUT);
+  }
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  updateSliderValues();
+  sendSliderValues(); // Actually send data (all the time)
+  // printSliderValues(); // For debug
+  delay(10);
+}
+
+void updateSliderValues() {
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+     analogSliderRead[i] = map(analogRead(analogInputs[i]), sliderMin, sliderMax, 0, 1023);
+     analogSliderValues[i] = constrain(analogSliderRead[i],0,1023);
+  }
+}
+
+void sendSliderValues() {
+  String builtString = String("");
+
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    builtString += String((int)analogSliderValues[i]);
+
+    if (i < NUM_SLIDERS - 1) {
+      builtString += String("|");
+    }
+  }
+  
+  Serial.println(builtString);
+}
+
+void printSliderValues() {
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    String printedString = String("Slider #") + String(i + 1) + String(": ") + String(analogSliderValues[i]) + String(" mV");
+    Serial.write(printedString.c_str());
+
+    if (i < NUM_SLIDERS - 1) {
+      Serial.write(" | ");
+    } else {
+      Serial.write("\n");
+    }
+  }
+}
